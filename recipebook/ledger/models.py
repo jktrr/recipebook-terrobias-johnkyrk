84874757673828421767bb59caf70fc64dcbbf4d
jktrr <jkterrobias@gmail.com>
2026-02-19 09:06:12 +0800

    basic models & code cleanup

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -6,5 +6,14 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
     
-# class Recipe(models.Model):
+class Recipe(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredients")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe_name")
 
